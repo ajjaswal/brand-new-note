31ee956322c281ccb3a0275295ce88ebcefdc158
AJ Jaswal <jaswalakshay@yahoo.com>
2022-06-10 23:37:48 -0400

    server.js changes

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/notes.html'))
 });
