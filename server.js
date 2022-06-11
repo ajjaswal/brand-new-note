ee0fd1f339ec88958c5f3c98eccf15c775c86887
AJ Jaswal <jaswalakshay@yahoo.com>
2022-06-10 23:39:18 -0400

    updated routes to use path

--- server.js
+++ server.js
@@ -10,19 +10,20 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true}));
-
 app.use(express.json());
-
 app.use(express.static("public"));
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/notes.html'))
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname, '/index.html'));
-});   
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
-app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${PORT}`);
+app.get("/api/notes", (req, res) => {
+    fs.readFile("db/db.json", 'utf-8', function(err, data) {
+        if (err) throw err;
+        res.json(JSON.parse(data))
+    })
 });
