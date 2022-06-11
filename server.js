f11e61a91bb233315e1d2774a6f2a939fc6e57b5
AJ Jaswal <jaswalakshay@yahoo.com>
2022-06-10 23:35:52 -0400

    file structure changes, routes added to server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const PORT = process.env.PORT || 8080;
+
 const express = require('express');
 const fs = require('fs');
+const { v4: uuidv4} = require('uuid')
+
 const path = require('path');
 
 const app = express();
+
+const PORT = process.env.PORT || 3001;
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/notes.html'))
@@ -16,4 +20,3 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
 });
-
