598bc2dc8ce79ab16c9bbb7ad8308cb4a84196a5
AJ Jaswal <jaswalakshay@yahoo.com>
2022-06-10 20:51:48 -0400

    added paths for notes, index and eventlistener for port

--- server.js
+++ server.js
@@ -5,3 +5,15 @@ const path = require('path');
 
 const app = express();
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/notes.html'))
+});
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, '/index.html'));
+});   
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
+
